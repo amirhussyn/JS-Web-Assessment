ab972201a78f74bbd8d6e94d5e5d2c23d1cab54a
amirhussyn <amir.hussyn@gmail.com>
2021-08-30 00:36:33 +0800

    Readme not updated

--- game.js
+++ game.js
@@ -2,11 +2,14 @@
 const prompt = require('prompt-sync')({sigint: true});
  
 let playing = true;
+let startingPoints = 100;
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+const marker = '+';
 const rowNum = 10, colNum = 10;
+
 
 
 class Field {
@@ -72,55 +75,152 @@ class Field {
     }
 
     gameDecision () {
-        if (this._field[this._locationY] == undefined) {
+        if (this._field[this._locationY] === undefined) {
             console.log("You can't go through walls fool!");
-            return playing = false;
+            playing = false;
         }
-        switch (this._field[this._locationY][this._locationX]) {
-            case hat:
-                console.log("You win! Congrats on finding your damn hat");
-                playing = false;
+        else {
+            switch (this._field[this._locationY][this._locationX]) {
+                case hat:
+                    console.log("You win! Congrats on finding your damn hat");
+                    playing = false;
+                    break;
+                case hole:
+                    console.log("You lose sucka!");
+                    playing = false;
+                    break;
+                case fieldCharacter:
+                    this._field[this._locationY][this._locationX] = pathCharacter;
+                    playing = true;
+                    break;
+                case pathCharacter:
+                    console.log("You stepped on the poison trail!");
+                    playing = false;
+                    break;
+                case undefined:
+                    console.log("You can't go through walls fool!");
+                    playing = false;
+                    break;
+                default:
+                    break;
+            }
+        }   
+    }
+
+    generateField2(percentage) {
+        for (let y = 0; y < rowNum; y++) {
+            for(let x = 0; x <colNum; x++) {
+                const prob = Math.random();
+                this._field[y][x] = prob > percentage ? fieldCharacter : hole; 
+            }
+        }
+
+        for (let y = 0; y < rowNum; y++) {
+            for(let x = 0; x <colNum; x++) {
+                const prob = Math.random();
+                if (prob < 0.09) {
+                    this._field[y][x] = marker;
+                }     
+            }
+        }
+
+        //Set the "hat" location : Object
+        const hatLocation = {
+            x: Math.floor(Math.random() * colNum),
+            y: Math.floor(Math.random() * rowNum)
+        };
+
+        //Make sure the "hat" is not at the starting point 
+        while (hatLocation.x == 0 && hatLocation.y == 0) {
+            hatLocation.x = Math.floor(Math.random() * colNum);
+            hatLocation.y = Math.floor(Math.random() * rowNum);
+        }
+
+        this._field[hatLocation.y][hatLocation.x] = hat;
+        this._field[0][0] = pathCharacter;
+    }
+
+    askQuestion2() {
+        const direction = prompt('Score: ' + startingPoints + ' Which way? ').toUpperCase();
+        switch (direction) {
+            case "U":
+                this._locationY = this._locationY - 1;
                 break;
-            case hole:
-                console.log("You lose sucka!");
-                playing = false;
+            case "D":
+                this._locationY = this._locationY + 1;
                 break;
-            case fieldCharacter:
-                this._field[this._locationY][this._locationX] = pathCharacter;
-                playing = true;
+            case "L":
+                this._locationX = this._locationX - 1;
                 break;
-            case pathCharacter:
-                console.log("You stepped on the poison trail!");
-                playing = false;
-                break;
-            case undefined:
-                console.log("You can't go through walls fool!");
-                playing = false;
+            case "R":
+                this._locationX = this._locationX + 1;
                 break;
             default:
                 break;
 
         }   
     }
-   
+    
+    gameDecision2() {
+        if (this._field[this._locationY] == undefined) {
+            console.log("You can't go through walls fool!");
+            playing = false;
+        }
+        else {
+            switch (this._field[this._locationY][this._locationX]) {
+                case hat:
+                    console.log("You win! Total Points: " + startingPoints);
+                    playing = false;
+                    break;
+                case hole:
+                    console.log("You lose sucka!");
+                    playing = false;
+                    break;
+                case fieldCharacter:
+                    this._field[this._locationY][this._locationX] = pathCharacter;
+                    startingPoints -= 10;
+                    playing = true;
+                    break;
+                case marker:
+                    this._field[this._locationY][this._locationX] = pathCharacter;
+                    startingPoints += 50;
+                    playing = true;
+                    break;
+                case pathCharacter:
+                    console.log("You stepped on the poison trail!");
+                    playing = false;
+                    break;
+                case undefined:
+                    console.log("You can't go through walls fool!");
+                    playing = false;
+                    break;
+                default:
+                    break;
+            
+            }
+        }
+        if (startingPoints == 0) {
+            console.log('Your points are kosong!');
+            playing = false;
+        }   
+    }
 
 }
 
 
 //Create an instance of Field Class Object 
 const myField = new Field();
-const difficulty = prompt('Please select a difficulty:\nEASY = E \nMEDIUM = M \nHARD = H\nKey in here => ').toUpperCase();
-if (difficulty == "E") {
-    myField.generateField(0.2);
-    startGame();
-}
-else if (difficulty == "M") {
+
+console.log('Please select a game mode\nNORMAL = N\nSPECIAL WITH POINTS = S');
+const gameMode = prompt('Enter letter here ').toUpperCase();
+
+if (gameMode == "N") {
     myField.generateField(0.3);
     startGame();
 }
-else if (difficulty == "H") {
-    myField.generateField(0.4);
-    startGame();
+else if (gameMode == "S") {
+    myField.generateField2(0.3);
+    startGame2();
 }
 
 
@@ -132,6 +232,19 @@ function startGame() {
         myField.print();
         myField.askQuestion();
         myField.gameDecision();
+    }
+    console.log("Thanks for playing!");
+
+}
+
+function startGame2() {
+
+    console.log("Start your engines!");
+
+    while (playing) {
+        myField.print();
+        myField.askQuestion2();
+        myField.gameDecision2();
     }
     console.log("Thanks for playing!");
 
